a8fa326e9e1d467da9c107089b6277cb7ce810b5
JosepRDA <josepaulorom@gmail.com>
2026-09-18 22:32:47 -0300

    refactor: added prototype for kmain in stdlib.h

--- src/lib/stdlib.h
+++ src/lib/stdlib.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+void kmain(void);
 void* memcpy(void* restrict dest, const void* restrict src, size_t n);
 void* memset(void* s, int c, size_t n);
 void* memmove(void* dest, const void* src, size_t n);
